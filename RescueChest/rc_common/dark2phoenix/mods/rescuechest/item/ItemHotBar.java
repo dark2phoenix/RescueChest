12818512ff23090e03263c580b9ddffa14422936
dark2phoenix <dark3phoenix@gmail.com>
2013-05-11 09:10:22 -0400

    Updated to Minecraft 1.5.2

---
+++ RescueChest/rc_common/dark2phoenix/mods/rescuechest/item/ItemHotBar.java
@@ -0,0 +1,40 @@
+package dark2phoenix.mods.rescuechest.item;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.util.Icon;
+
+public class ItemHotBar extends Item {
+
+    public ItemHotBar(int id) {
+        super(id);
+    }
+
+ 
+    /**
+     * Icon list for the GUI to show whether slots are enabled or not
+     */
+    @SideOnly(Side.CLIENT)
+    private static Icon[] hotbarIcons = new Icon[2];
+    
+    @SideOnly(Side.CLIENT)
+    public static Icon getHotbarIcon( int offset ) {
+        return hotbarIcons[offset];
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public static Icon setHotbarIcon( int offset, Icon icon ) {
+        return hotbarIcons[offset] = icon;
+    }
+    
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
+        setHotbarIcon(0, iconRegister.registerIcon("RescueChest:SlotAvailable"));
+        setHotbarIcon(1, iconRegister.registerIcon("RescueChest:SlotNotAvailable"));
+    }
+
+
+}
